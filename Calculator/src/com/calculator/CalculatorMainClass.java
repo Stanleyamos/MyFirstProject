79eb1672199caa2eee429e3155c69e0e96158800
user <user@DESKTOP-BCEQ1E1>
2023-09-10 16:25:51 +0900

    Project Setup

---
+++ Calculator/src/com/calculator/CalculatorMainClass.java
@@ -0,0 +1,12 @@
+package com.calculator;
+
+public class CalculatorMainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Calculate numbers");
+		System.out.print("Calculate numbers");
+
+	}
+
+}
